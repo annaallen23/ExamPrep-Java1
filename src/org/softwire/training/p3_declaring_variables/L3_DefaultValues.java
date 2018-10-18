2c1feb0a449c71150027f372843c818197789235
James Griffiths <james.griffiths@softwire.com>
2018-10-18 16:01:11 +0100

    Declaring variables

---
+++ src/org/softwire/training/p3_declaring_variables/L3_DefaultValues.java
@@ -0,0 +1,36 @@
+package org.softwire.training.p3_declaring_variables;
+
+public class L3_DefaultValues {
+
+    public static void main(String[] args) {
+
+        // ????????????????????????
+        // Which of these are valid
+        // ????????????????????????
+
+//        int intOne = 1;
+//        System.out.println("intOne: " + intOne);
+
+//        int intTwo;
+//        intTwo = 2;
+//        System.out.println("intTwo: " + intTwo);
+
+//        int intThree;
+//        System.out.println("intThree: " + intThree);
+
+//        Tiger tiger = new Tiger();
+//        System.out.println("tiger.age: " + tiger.age);
+
+//        System.out.println("Badger.population: " + Badger.population);
+
+    }
+}
+
+
+class Tiger {
+    public int age;
+}
+
+class Badger {
+    public static int population;
+}
---
+++ src/org/softwire/training/p3_declaring_variables/L4_VariableScope.java
@@ -0,0 +1,57 @@
+package org.softwire.training.p3_declaring_variables;
+
+public class L4_VariableScope {
+
+    // ???????????????
+    // Will this work?
+    // ???????????????
+    private static void calculateDiscountOne(int price) {
+
+//        if (price > 100) {
+//            int discount = 20;
+//        }
+//        else {
+//            int discount = 5;
+//        }
+//
+//        System.out.println(price);
+//        System.out.println(discount);
+    }
+
+
+    // ???????????????
+    // Will this work?
+    // ???????????????
+    private static void calculateDiscountTwo(int price) {
+
+//        int discount;
+//        if (price > 100) {
+//            discount = 20;
+//        }
+//        else {
+//            discount = 5;
+//        }
+//
+//        System.out.println(price);
+//        System.out.println(discount);
+    }
+
+
+    // ???????????????
+    // Will this work?
+    // ???????????????
+    private static void calculateDiscountThree(int price) {
+
+//        int discount;
+//        if (price > 100) {
+//            int discount = 20;
+//        }
+//        else {
+//            int discount = 5;
+//        }
+//
+//        System.out.println(price);
+//        System.out.println(discount);
+    }
+
+}
